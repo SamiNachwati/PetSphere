216d1f4129c635aca9c93929094d5a748a5e2cdb
Sami <saminachwati94@gmail.com>
2024-04-23 13:19:54 -0400

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-//StAuth10244: I Sami Nachwati, 000879289 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
